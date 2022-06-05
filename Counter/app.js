64df81fe05b65ac0143b9836c87957cc4d6dca1e
Konstantin Pavlov <konstantin94pavlov@gmail.com>
2022-06-05 18:16:40 +0300

    added comments

--- Counter/app.js
+++ Counter/app.js
@@ -4,18 +4,21 @@ const increase = document.querySelector(".increase-button");
 const decrease = document.querySelector(".decrease-button");
 const reset = document.querySelector(".reset-button");
 
+//увеличить на 1
 increase.addEventListener("click", function () {
   let value = num.textContent;
   value = parseInt(value) + 1;
   num.textContent = value;
 });
 
+//уменьшить на 1
 decrease.addEventListener("click", function () {
   let value = num.textContent;
   value = parseInt(value) - 1;
   num.textContent = value;
 });
 
+//обнулить
 reset.addEventListener("click", function () {
   num.textContent = 0;
 });
