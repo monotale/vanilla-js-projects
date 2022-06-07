89e572c80e60bcc7bbaf80f901a6104459aac746
Konstantin Pavlov <konstantin94pavlov@gmail.com>
2022-06-08 01:00:15 +0300

    Finished "stupid" method to change buttons

--- Review/app.js
+++ Review/app.js
@@ -15,33 +15,62 @@ const reviews = [
     review:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, blanditiis minima? Voluptates repellendus quo corporis nam sint, dolorem est laudantium recusandae deserunt voluptatibus sequi eius incidunt. Eligendi est ut magni.",
   },
+  {
+    id: 3,
+    fullName: "Mari Robertson",
+    profession: "product owner",
+    img: "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    review:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, blanditiis minima? Voluptates repellendus quo corporis nam sint, dolorem est laudantium recusandae deserunt voluptatibus sequi eius incidunt. Eligendi est ut magni.",
+  },
+  {
+    id: 4,
+    fullName: "Grag Mikkelson",
+    profession: "back-end developer",
+    img: "https://images.unsplash.com/photo-1628890920690-9e29d0019b9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGF2YXRhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+    review:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, blanditiis minima? Voluptates repellendus quo corporis nam sint, dolorem est laudantium recusandae deserunt voluptatibus sequi eius incidunt. Eligendi est ut magni.",
+  },
+  {
+    id: 5,
+    fullName: "Julia Assange",
+    profession: "analyst",
+    img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDF8fGF2YXRhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+    review:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, blanditiis minima? Voluptates repellendus quo corporis nam sint, dolorem est laudantium recusandae deserunt voluptatibus sequi eius incidunt. Eligendi est ut magni.",
+  },
+  {
+    id: 6,
+    fullName: "Mick Matters",
+    profession: "front-end developer",
+    img: "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    review:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, blanditiis minima? Voluptates repellendus quo corporis nam sint, dolorem est laudantium recusandae deserunt voluptatibus sequi eius incidunt. Eligendi est ut magni.",
+  },
 ];
 
 const fullName = document.querySelector(".fullname");
 const profession = document.querySelector(".profession");
-let img = document.querySelector(".image");
+const img = document.querySelector(".image");
 const review = document.querySelector(".review-text");
+const buttons = document.querySelectorAll(".btn");
 
-let currentItem = reviews[0];
+let currentPosition = 0;
 
 document.addEventListener("DOMContentLoaded", function () {
-  updateReview(currentItem);
+  updateReview(reviews[currentPosition]);
 });
 
-const buttons = document.querySelectorAll(".btn");
-console.log(buttons);
-
 buttons.forEach((element) => {
-  console.log(element.className);
   element.addEventListener("click", function (e) {
     const classes = e.currentTarget.classList;
+
     if (classes.contains("button-left")) {
-      //   console.log(classes.contains("button-left"));
-      updateReview(reviews[0]);
+      if (currentPosition > 0) updateReview(reviews[(currentPosition -= 1)]);
     }
     if (classes.contains("button-right")) {
-      //   console.log(classes.contains("button-right"));
-      updateReview(reviews[1]);
+      if (currentPosition < reviews.length - 1)
+        updateReview(reviews[(currentPosition += 1)]);
     }
   });
 });
