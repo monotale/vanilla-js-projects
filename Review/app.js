9385aeb362b42d477d0730bcfcdf18552cbe9b0c
Konstantin Pavlov <konstantin94pavlov@gmail.com>
2022-06-07 22:22:58 +0300

    started review app

---
+++ Review/app.js
@@ -0,0 +1,9 @@
+const reviews = [
+  {
+    id: 1,
+    fullname: "John Smith",
+    profession: "UI/UX designer",
+    review:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, blanditiis minima? Voluptates repellendus quo corporis nam sint, dolorem est laudantium recusandae deserunt voluptatibus sequi eius incidunt. Eligendi est ut magni.",
+  },
+];
