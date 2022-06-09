eea477400047f0d2e429837eafea9d56dc872198
Konstantin Pavlov <konstantin94pavlov@gmail.com>
2022-06-09 15:02:23 +0300

    Finished review

--- Review/app.js
+++ Review/app.js
@@ -5,7 +5,7 @@ const reviews = [
     profession: "UI/UX designer",
     img: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
     review:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, blanditiis minima? Voluptates repellendus quo corporis nam sint, dolorem est laudantium recusandae deserunt voluptatibus sequi eius incidunt. Eligendi est ut magni.",
+      "L consectetur adipisicing elit. Rem, blanditiis minima? Voluptates repellendus quo corporis nam sint, dolorem est laudantium recusandae deserunt voluptatibus sequi eius incidunt. Eligendi est ut magni.",
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ const reviews = [
     profession: "web designer",
     img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
     review:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, blanditiis minima? Voluptates repellendus quo corporis nam sint, dolorem est laudantium recusandae deserunt voluptatibus sequi eius incidunt. Eligendi est ut magni.",
+      "Nam sint, dolorem est laudantium recusandae deserunt voluptatibus sequi eius incidunt. Eligendi est ut magni.",
   },
   {
     id: 3,
@@ -29,7 +29,7 @@ const reviews = [
     profession: "back-end developer",
     img: "https://images.unsplash.com/photo-1628890920690-9e29d0019b9b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGF2YXRhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
     review:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, blanditiis minima? Voluptates repellendus quo corporis nam sint, dolorem est laudantium recusandae deserunt voluptatibus sequi eius incidunt. Eligendi est ut magni.",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Remt magni.",
   },
   {
     id: 5,
@@ -37,7 +37,7 @@ const reviews = [
     profession: "analyst",
     img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDF8fGF2YXRhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
     review:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, blanditiis minima? Voluptates repellendus quo corporis nam sint, dolorem est laudantium recusandae deserunt voluptatibus sequi eius incidunt. Eligendi est ut magni.",
+      "Lorem ipsum dot, dolorem est laudantium recusandae deserunt voluptatibus sequi eius incidunt. Eligendi est ut magni.",
   },
   {
     id: 6,
@@ -45,7 +45,7 @@ const reviews = [
     profession: "front-end developer",
     img: "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
     review:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rem, blanditiis minima? Voluptates repellendus quo corporis nam sint, dolorem est laudantium recusandae deserunt voluptatibus sequi eius incidunt. Eligendi est ut magni.",
+      "Lorem ilanditiis minima? Voluptates repellendus quo corporis nam sint, dolorem est laudantium recusandae deserunt voluptatibus sequi eius incidunt. Eligendi est ut magni.",
   },
 ];
 
@@ -54,6 +54,7 @@ const profession = document.querySelector(".profession");
 const img = document.querySelector(".image");
 const review = document.querySelector(".review-text");
 const buttons = document.querySelectorAll(".btn");
+const random = document.querySelector(".random-btn");
 
 let currentPosition = 0;
 
@@ -81,3 +82,11 @@ const updateReview = (target) => {
   img.src = target.img;
   review.textContent = target.review;
 };
+
+const randomReview = () => {
+  return Math.floor(Math.random() * reviews.length);
+};
+
+random.addEventListener("click", function () {
+  updateReview(reviews[randomReview()]);
+});
